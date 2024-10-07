7c9594cb9f94db1695ae356183cb3dc078687102
Nouhx <oscarfcm25@gmail.com>
2024-10-07 09:50:32 -0500

    Ejercicio de variables en Jscript

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+var nombre="";
+var edad=0;
+
+nombre=prompt("Por favor ingresa su nombre: ");
+edad=Number(prompt("Por favor ingrese su edad: "));
+var ciudad="Quito";
+
+console.log("Tu nombre es: "+nombre+", tu edad es: "+edad+", y la ciudad donde vives es: "+ciudad);
+alert("Hola: "+nombre+", tienes "+edad+" años y vives en "+ciudad);
